ffa009f2477c883cf445e5e4aebc1cbd8e9bc463
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-10 01:34:12 +0100

    Updated Controls component.

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,40 +1,71 @@
 import React from "react";
 import styles from "./styles.scss";
-import {Box, Button, Container, Grid, Typography} from "@material-ui/core";
-import {Form} from "./Form";
+import {Box, Container, Grid, Typography} from "@material-ui/core";
+import {scoreboardStore} from "../../services";
+import {Scoreboard} from "../../services/scoreboard";
+import {Subscription} from "rxjs";
+import {SelectKind} from "../index";
+import {Play} from "./play";
 
-class Controls extends React.PureComponent {
+interface ControlsState {
+    player: number;
+    opponent: number;
+}
+
+class Controls extends React.PureComponent<{}, ControlsState> {
+    private scoreboardSubscription: Subscription | undefined;
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            player: 0,
+            opponent: 0,
+        };
+    }
+
+    componentDidMount() {
+        this.scoreboardSubscription = scoreboardStore.subscription().subscribe(this.handleScoreboardSubscription);
+    }
+
+    componentWillUnmount() {
+        if (this.scoreboardSubscription === undefined) {
+            return;
+        }
+
+        this.scoreboardSubscription.unsubscribe();
+    }
+
+    handleScoreboardSubscription = (next: Scoreboard) => {
+        const { player, opponent } = next;
+        this.setState({ player, opponent });
+    }
+
     render() {
+        const { player, opponent } = this.state;
         return(
             <div className={styles.Controls}>
                 <Container>
                     <Grid container spacing={3}>
                         <Grid item xs={12} md={6}  lg={4}>
                             <Box component="span" m={1}>
-                                <Button variant="contained" color="secondary">
-                                    New battle ⚔️
-                                </Button>
-                            </Box>
-                            <Box component="span" m={1}>
-                                <Button variant="contained">
-                                    Reset 🧹
-                                </Button>
+                                <Play />
                             </Box>
                         </Grid>
                         <Grid item xs={12} md={6} lg={4}>
                             <Box component="span" m={1}>
                                 <Typography display="inline" variant="body2">
-                                    Wins: <span>#</span>
+                                    Player: <span>{player}</span>
                                 </Typography>
                             </Box>
                             <Box component="span" m={1}>
                                 <Typography display="inline" variant="body2">
-                                    Loss: <span>#</span>
+                                    Opponent: <span>{opponent}</span>
                                 </Typography>
                             </Box>
                         </Grid>
                         <Grid item xs={12} md={6}  lg={4}>
-                            <Form />
+                            <SelectKind />
                         </Grid>
                     </Grid>
                 </Container>
