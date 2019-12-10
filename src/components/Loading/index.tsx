73de2d12841efb4dc4681c00dbd8f0cc88b7f134
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-10 21:53:01 +0100

    Added Loading screen component.

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {Button, Container, Typography} from "@material-ui/core";
+import * as styles from "./styles.scss";
+import {Main} from "../../layout";
+import {TopBar} from "../index";
+import Link from "next/link";
+
+interface LoadingProps {
+    content?: string;
+}
+
+const Loading: React.FunctionComponent<LoadingProps> = props => {
+    const { content } = props;
+
+    return(
+        <Main>
+            <TopBar />
+            <Container>
+                <div className={styles.Loading}>
+                    <div className={styles.Content}>
+                        <Typography variant="h5" color="textSecondary" align="center">{content}</Typography>
+                    </div>
+                    <Link href="/" >
+                        <Button variant="contained">Restart</Button>
+                    </Link>
+                </div>
+            </Container>
+        </Main>
+    );
+};
+
+Loading.defaultProps = {
+    content: "Loading…"
+};
+
+export default Loading;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Details } from "./Details";
 export { default as SelectPlayer } from "./SelectPlayer";
 export { default as SelectKind } from "./SelectKind";
 export { default as Link } from "./Link";
+export { default as Loading } from "./Loading";
