02aab8ff8a315c242c904b921a326aa7586e1e08
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-10 01:36:11 +0100

    COMPONENTS INDEX.TS UPDATE

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { default as TopBar } from "./TopBar/";
 export { default as Controls } from "./Controls/";
 export { default as Details } from "./Details";
+export { default as SelectPlayer } from "./SelectPlayer";
+export { default as SelectKind } from "./SelectKind";
+export { default as Link } from "./Link";
