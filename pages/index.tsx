91b7e393bf535b7eb81985816a9f550d625edba7
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-10 01:39:38 +0100

    Caching API, new Components, index page updates.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,47 @@
 import React from "react";
 import { Main } from "../src/layout";
 import styles from "./index.scss";
-import { NextPage } from "next";
-import { Typography } from "@material-ui/core";
-import {Controls, TopBar} from "../src/components";
+import {Fab, Typography} from "@material-ui/core";
+import GamesIcon from "@material-ui/icons/Games";
+import {TopBar} from "../src/components";
+import Link from "next/link";
+import {SwApi} from "../src/dao";
+import {ResultListResponse} from "../src/dao/types";
 
 interface HomeProps {
-    userAgent: string
+    apiData: ResultListResponse;
 }
 
-const Home: NextPage<HomeProps> = props => {
-    const { userAgent } = props;
+class Home extends React.Component<HomeProps> {
+    static async getInitialProps() {
+        const { resultList: apiData } = await new SwApi().getResults();
+        return { apiData };
+    }
 
-    return (
-        <Main>
-            <main>
-                <TopBar />
-                <Controls />
-                <h1 className={styles.helloWorld}>Hello world!</h1>
-                <Typography variant="body2" color="textSecondary" align="center">user agent: <span>{userAgent}</span></Typography>
-            </main>
-        </Main>
-    );
-};
+    componentDidMount() {
+        const { apiData } = this.props;
 
-Home.getInitialProps = async ({ req }) => {
-    const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
-    return { userAgent };
+        localStorage.setItem("apiDataSaved", JSON.stringify(apiData));
+    }
+
+    render() {
+        return (
+            <Main>
+                <main>
+                    <TopBar/>
+                    <div className={styles.playWrapper}>
+                        <Link href="/play?kind=people&id=0&idOpponent=0" as="/play/people/0/0/">
+                            <Fab variant="extended">
+                                <GamesIcon/>
+                                Play
+                            </Fab>
+                        </Link>
+                    </div>
+                    <Typography variant="body2" color="textSecondary" align="center">Amadeusz Blanik &copy 2019</Typography>
+                </main>
+            </Main>
+        );
+    }
 };
 
 export default Home;
--- pages/play.tsx
+++ pages/play.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Main } from "../src/layout";
 import styles from "./index.scss";
 import {Container, Grid, Typography} from "@material-ui/core";
-import {Controls, Details, TopBar} from "../src/components";
+import {Controls, Details, TopBar, SelectPlayer} from "../src/components";
 import {KIND, ResultListResponse, ResultListResponseSingle} from "../src/dao/types";
-import { SwApi } from "../src/dao/";
+import {scoreboardStore} from "../src/services";
 
-type RESULT_SCORE = "WIN" | "LOSE" | "DRAW";
+type RESULT_SCORE = "player" | "opponent" | "draw";
 
-interface HomeProps {
-    apiData: ResultListResponse;
+interface PlayProps {
     id: number;
     idOpponent: number;
     kind: KIND;
+}
+
+interface PlayState {
+    apiData: ResultListResponse | false;
+    currentResult: RESULT_SCORE;
 }
 
 interface HomeGetInitialProps {
@@ -24,15 +28,49 @@ interface HomeGetInitialProps {
     req: any;
 }
 
-class Play extends React.Component<HomeProps, any> {
+class Play extends React.Component<PlayProps, PlayState> {
+    constructor(props: PlayProps) {
+        super(props);
+
+        this.state = { apiData: false, currentResult: "draw" };
+    }
+
     static async getInitialProps({ query: { kind, id, idOpponent } }: HomeGetInitialProps) {
-        const { resultList: apiData } = await new SwApi().getResults();
-        return { apiData, id, idOpponent, kind };
+        return { id, idOpponent, kind };
+    }
+
+    componentDidMount() {
+        const apiDataInLocalStorage = localStorage.getItem("apiDataSaved");
+
+        if (!apiDataInLocalStorage) {
+            throw new Error("No api data in localStorage!");
+        }
+
+        this.setState({ apiData: JSON.parse(apiDataInLocalStorage) });
+
+        const result: RESULT_SCORE = this.getResult();
+
+        if (result === "player") {
+            scoreboardStore.addPointPlayer();
+        } else if (result === "opponent") {
+            scoreboardStore.addPointOpponent();
+        } else if (result === "draw") {
+            scoreboardStore.addPointDraw();
+        }
+
+        this.setState({ currentResult: result });
     }
 
     getResult = () => {
-        const { apiData, kind, id, idOpponent } = this.props;
-        const currentKindData: ResultListResponseSingle | undefined = apiData[kind!]
+        const { kind, id, idOpponent } = this.props;
+        const { apiData } = this.state;
+
+        if (!apiData) {
+            console.warn('no data api');
+            return "draw";
+        }
+
+        const currentKindData: ResultListResponseSingle | undefined = apiData[kind!];
 
         const score = {
             player: 0,
@@ -40,7 +78,7 @@ class Play extends React.Component<HomeProps, any> {
         };
 
         if (typeof currentKindData === "undefined") {
-            return "DRAW";
+            return "draw";
         }
 
         const playerData = currentKindData.list[id].data;
@@ -53,19 +91,25 @@ class Play extends React.Component<HomeProps, any> {
             score.player = Number(playerData.crew);
             score.opponent = Number(opponentData.crew);
         } else {
-            return "DRAW";
+            return "draw";
         }
 
         const isAnyNaN: boolean = isNaN(score.player) || isNaN(score.opponent);
-        const result: RESULT_SCORE = !isAnyNaN ? score.player >= score.opponent ? score.player !== score.opponent ? "WIN" : "DRAW" : "LOSE" : "DRAW";
+        const result: RESULT_SCORE = !isAnyNaN ? score.player >= score.opponent ? score.player !== score.opponent ? "player" : "draw" : "opponent" : "draw";
 
         return result;
     }
 
     render() {
-        const { apiData, kind } = this.props;
+        const { kind, id, idOpponent } = this.props;
+        const { apiData } = this.state;
+
+        if (!apiData) {
+            return (<>You need to init app first</>);
+        }
+
         const result: RESULT_SCORE = this.getResult();
-        const currentKindData: ResultListResponseSingle | undefined = apiData[kind!]
+        const currentKindData: ResultListResponseSingle | undefined = apiData[kind!];
 
         console.log("Play(render)::", { currentKindData });
 
@@ -76,6 +120,12 @@ class Play extends React.Component<HomeProps, any> {
                     <Controls />
                     <h1 className={styles.helloWorld}>You {result}</h1>
                     <Grid container spacing={3}>
+                        <Grid item xs={12} md={6}>
+                            <SelectPlayer type="player" list={typeof currentKindData !== "undefined" ? currentKindData.list : []} initialValue={id} />
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <SelectPlayer type="opponent" list={typeof currentKindData !== "undefined" ? currentKindData.list : []} initialValue={idOpponent} />
+                        </Grid>
                         <Grid item xs={12} md={6}>
                             <Details/>
                         </Grid>
