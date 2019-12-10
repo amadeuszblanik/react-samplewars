780726ed50e937156ff0b54424bb6405bb229194
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-10 01:37:23 +0100

    Updated theme settings.

--- src/settings/theme.ts
+++ src/settings/theme.ts
@@ -1,19 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
+import {deepPurple, red} from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
     palette: {
-        primary: {
-            main: "#556cd6",
-        },
-        secondary: {
-            main: "#19857b",
-        },
-        error: {
-            main: red.A400,
-        },
-        background: {
-            default: "#fff",
-        },
+        primary: deepPurple,
+        secondary: red,
     },
 });
