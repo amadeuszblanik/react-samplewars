11fb9fb56eb6c4ff3e2c230257cd05c561260985
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-11 21:58:57 +0100

    Added dateDiff utils

---
+++ src/utils/dateDiff.ts
@@ -0,0 +1,18 @@
+type DATE = number | Date;
+
+const makeNumber = (value: DATE) => {
+    if (typeof value !== "number") {
+        value = value.getTime();
+    }
+    return value;
+};
+
+const daysDiff = (dateA: DATE, dateB: DATE) => {
+    return Math.round(Math.abs(makeNumber(dateB) - makeNumber(dateA)) / 86400000);
+};
+
+const hoursDiff = (dateA: DATE, dateB: DATE) => {
+    return Math.round(Math.abs(makeNumber(dateB) - makeNumber(dateA)) / 3600000);
+};
+
+export { daysDiff, hoursDiff };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as forEachObject } from "./forEachObject";
 export { default as getRandomNumber } from "./getRandomNumber";
+export { daysDiff, hoursDiff } from "./dateDiff";
