d2875faa956efd026929d349bd25d34e5665920e
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-16 00:24:38 +0100

    Added Theme Provider for Styled Components.

--- src/settings/theme.ts
+++ src/settings/theme.ts
@@ -1,5 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { deepPurple } from "@material-ui/core/colors";
+import { blue, deepPurple } from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
   palette: {
@@ -7,3 +7,14 @@ export const theme = createMuiTheme({
     type: "dark",
   },
 });
+
+export const styledTheme = {
+  palette: {
+    primary: deepPurple["400"],
+    secondary: blue["400"],
+  },
+  zIndex: {
+    z10: 1000,
+    z20: 1020,
+  },
+};
