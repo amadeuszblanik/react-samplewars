a4f23aeda64cb4e2a47561b3c1f17a9e9f711f30
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-15 17:18:06 +0100

    Added new components to Game module.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
 export { default as TopBar } from "./TopBar/";
-export { default as Controls } from "./Controls/";
 export { default as Details } from "./Details";
 export { default as SelectKind } from "./SelectKind";
 export { default as Link } from "./Link";
 export { default as Loading } from "./Loading";
 export { default as SelectCharacter } from "./SelectCharacter";
+export { default as ScoreboardComponent } from "./Scoreboard";
+export { default as Controls } from "./Controls";
--- src/modules/Game/index.tsx
+++ src/modules/Game/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Controls, Details, SelectCharacter, TopBar } from "../../components";
+import styled from "styled-components";
+import { Controls, Details, ScoreboardComponent, SelectCharacter, SelectKind, TopBar } from "../../components";
 import { Container, Grid } from "@material-ui/core";
 import { Main } from "../../layout";
-import { withApiData } from "../../utils";
+import { getDataOfKind, withApiData } from "../../utils";
 import { InjectedWithApiData } from "../../utils/withApiData";
 import { KIND } from "../../dto";
 
@@ -10,14 +11,36 @@ interface GamecoreProps extends InjectedWithApiData {
   kind: KIND;
 }
 
+const Row = styled(Grid)`
+  padding-top: 24px;
+`;
+
+const Col = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
 const Game: React.FunctionComponent<GamecoreProps> = props => {
   const { kind, apiData } = props;
+  const dataCurrentKind = getDataOfKind(apiData, kind);
 
   return (
     <Main>
       <TopBar />
       <Container>
-        <Controls kind={kind} data={apiData} />
+        <Row container spacing={3}>
+          <Col item xs={4} md={4}>
+            <Controls data={dataCurrentKind} />
+          </Col>
+          <Col item xs={8} md={4}>
+            <ScoreboardComponent />
+          </Col>
+          <Col item xs={12} md={4}>
+            <SelectKind kind={kind} />
+          </Col>
+        </Row>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
             <SelectCharacter type="player" data={apiData} />
