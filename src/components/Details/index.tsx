d69de5512294f2ae351ab221c288689c704a696f
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-15 17:16:12 +0100

    Details with settings

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,36 +1,52 @@
 import React from "react";
 import styles from "./styles.scss";
-import {Card, CardContent, Typography} from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import List from "./List";
-import {DATA} from "./types";
+import { withSettings } from "../../services";
+import { getDetailsOfId } from "../../utils";
+import { InjectedWithSettingsProps } from "../../services/withSettings";
+import { ResultListResponse } from "../../dto";
+import { TYPE } from "../SelectCharacter";
 
-interface DetailsProps {
-    title: "player" | "opponent";
-    data?: DATA;
+interface DetailsProps extends InjectedWithSettingsProps {
+  type: TYPE;
+  data: ResultListResponse;
 }
 
 const Details: React.FunctionComponent<DetailsProps> = props => {
-    const { data, title } = props;
+  const {
+    data,
+    type,
+    settings: {
+      kind,
+      player: { id },
+      opponent: { id: idOpponent },
+    },
+  } = props;
 
-    if (data === undefined) {
-        return <></>;
-    }
+  const currentId = type === "player" ? id : idOpponent;
 
-    return(
-        <div className={styles.Details}>
-            <Card>
-                <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                            Character of {title}
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        {data.name}
-                    </Typography>
-                    <List items={data!} />
-                </CardContent>
-            </Card>
-        </div>
-    );
+  const dataCurrent = getDetailsOfId(data, currentId, kind);
+
+  if (dataCurrent === undefined) {
+    return <></>;
+  }
+
+  return (
+    <div className={styles.Details}>
+      <Card>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Character of {type}
+          </Typography>
+          <Typography variant="h5" component="h2">
+            {dataCurrent.name}
+          </Typography>
+          <List items={dataCurrent!} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
-export default Details;
+export default withSettings(Details);
