7cd13562b3b6bb869294323edc0412f67884daab
Amadeusz Blanik <amadeusz@blanik.me>
2019-12-16 00:43:26 +0100

    eslint fixes

--- server.js
+++ server.js
@@ -9,29 +9,29 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const ssrCache = cacheableResponse({
-    ttl: !dev ? 1000 * 60 * 60 * 8 : 1,
-    get: async ({ req, res, pagePath, queryParams }) => ({
-        data: await app.renderToHTML(req, res, pagePath, queryParams)
-    }),
-    send: ({ data, res }) => res.send(data)
+  ttl: !dev ? 1000 * 60 * 60 * 8 : 1,
+  get: async ({ req, res, pagePath, queryParams }) => ({
+    data: await app.renderToHTML(req, res, pagePath, queryParams),
+  }),
+  send: ({ data, res }) => res.send(data),
 });
 
 app.prepare().then(() => {
-    const server = express();
+  const server = express();
 
-    server.get("/", (req, res) => ssrCache({ req, res, pagePath: "/" }));
+  server.get("/", (req, res) => ssrCache({ req, res, pagePath: "/" }));
 
-    server.get("/play/:kind/", (req, res) => {
-        const queryParams = { kind: req.params.kind };
-        const pagePath = "/play";
-        return ssrCache({ req, res, pagePath, queryParams });
-    });
+  server.get("/play/:kind/", (req, res) => {
+    const queryParams = { kind: req.params.kind };
+    const pagePath = "/play";
+    return ssrCache({ req, res, pagePath, queryParams });
+  });
 
-    server.get("*", (req, res) => handle(req, res));
+  server.get("*", (req, res) => handle(req, res));
 
-    server.listen(port, err => {
-        if (err) throw err;
-        console.log(`> Ready on http://localhost:${port} 🌑`);
-        console.log("> May the Force be with you 🚀 ");
-    });
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port} 🌑`);
+    console.log("> May the Force be with you 🚀 ");
+  });
 });
